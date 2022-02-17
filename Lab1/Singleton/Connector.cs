a99baa37a3092a286d0a0c75a11615e45f08a0e2
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2022-02-17 18:12:07 +0300

    Lab1 (Singleton added)

---
+++ Lab1/Singleton/Connector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DAOSS
+{
+    public class Connector
+    {
+        public Connector()
+        {
+            var config = Config.Instance;
+            Console.WriteLine("From Connector constructor:");
+            Console.WriteLine(config.ToString());
+        }
+    }
+}
